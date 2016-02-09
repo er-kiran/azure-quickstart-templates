69f69e9aa59c59d307eb3fca0383375ac2446802
Kiran Nagaraja <kiran.nagaraja@ericsson.com>
2016-02-09 12:00:31 -0800

    Sample app init setup script to set up required app packages etc.

---
+++ 201-customscript-sshkey-ubuntu/sample-java-app-init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get -y -q install software-properties-common htop
+
+# add oracle java8 repo
+add-apt-repository ppa:webupd8team/java
+
+sudo apt-get -yq update
+
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
+apt-get -y -q install oracle-java8-installer
+
+update-java-alternatives -s java-8-oracle
